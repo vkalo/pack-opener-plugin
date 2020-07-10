4cb547c99bbf71b2c77a37959d754f6bfe8d3697
likalo <904997417@qq.com>
2020-07-10 13:35:49 +0800

    1.0.5 增加模块缓存功能

--- src/outModule.js
+++ src/outModule.js
@@ -1,12 +1,40 @@
 
-const { outfiles} = require('./init');
+const { outfiles, moduleList, moduleName } = require('./init');
 const uglifyjs = require("uglify-es");
 const options = { toplevel: true };
+const { join } = require('path');
+const { readdirSync, readFileSync, writeFileSync,mkdirSync,existsSync } = require('fs');
+
+const moduleCachePath = join(__dirname, '../moduleCache');
+if (!existsSync(moduleCachePath)) {
+  mkdirSync(moduleCachePath);
+}
 
 function exportModule() {
+  let { [moduleName]: selfModule, ...nodeModule } = moduleList;
+  let nodeModuleList = {};
+  Object.values(nodeModule).forEach(item => {
+    nodeModuleList[item.modulePath] = item;
+  });
+  const moduleCache = readdirSync(moduleCachePath);
+
   return Object.entries(outfiles).reduce((out, [path, text]) => {
     if (path.endsWith('.js')) {
-      out[path] = uglifyjs.minify(text, options).code;
+      let text = null;
+
+      if (path in nodeModuleList) {
+        const cacheName = nodeModuleList[path].moduleName + '.js';
+        if (moduleCache.includes(cacheName)) {
+          text = readFileSync(join(moduleCachePath, cacheName))
+        } else {
+          text = uglifyjs.minify(text, options).code;
+          writeFileSync(join(moduleCachePath, cacheName), text);
+        }
+      } else {
+        text = uglifyjs.minify(text, options).code;
+      }
+
+      out[path] = text;
     } else {
       out[path] = text;
     }
